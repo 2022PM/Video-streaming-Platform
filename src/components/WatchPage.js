c8d87fd4211903d3e7d22fd9661871b441cf1a1a
2022PM <melkanip98@gmail.com>
2023-03-05 18:08:55 +0530

    Major Part1

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,21 +2,22 @@ import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux';
 import { useSearchParams } from 'react-router-dom';
 import { closeMenu } from '../utils/appSlice';
+import CommentsContainer from './CommentsContainer';
 
 const WatchPage = () => {
   const[searchParams]=useSearchParams();
-  console.log(searchParams.get('v'));
+ 
   const dispatch=useDispatch();
   useEffect(()=>{
     dispatch(closeMenu())
   },[]);
   
   return (
+    <div className='flex flex-col'>
     <div>
-        
         <iframe width="560" height="315" src={"https://www.youtube.com/embed/"+searchParams.get('v')} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-
-
+    </div>
+    <CommentsContainer/>
     </div>
     )
 }
